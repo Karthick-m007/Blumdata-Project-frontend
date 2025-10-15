e45e498bfaacdcedf6be02e6b478784486f47e61
Karthick-m007 <karthickraja200211@gmail.com>
2025-10-15 11:07:57 +0530

    url admin change

--- src/Components/Loginpage.js
+++ src/Components/Loginpage.js
@@ -72,7 +72,7 @@ const LoginPage = () => {
                         console.log("Admin Data can be accessed")
                         console.log("Admin:", isAdmin)
                         // Admin login is successful
-                        navigate('/admin');
+                        navigate('/admindashboard');
                     } else {
                         // User login is successful
                         navigate('/userdashboard');
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -38,7 +38,7 @@ export default function Navbar() {
     };
 
     const menuItems = [
-        { label: 'Dashboard', path: '/admin' },
+        { label: 'Dashboard', path: '/admindashboard' },
         { label: 'Manage Products', path: '/manageproduct' },
         { label: 'Manage Quotes', path: '/quotmanage' },
         // { label: 'Track Orders', path: '/admintrack' },
